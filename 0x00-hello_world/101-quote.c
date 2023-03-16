39de3ba26b7d85b2f6287d10a70b7edaf6a36580
RaveenaBora <raveenabora4@gmail.com>
2023-03-16 13:48:58 -0700

    program that return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19";
-	const size_t message_len = sizeof(message) - 1;
-	write(STDOUT_FILENO, message, message_len);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
