fb57261004d13c9b4fa77c3d0d71ed272f32feda
RaveenaBora <raveenabora4@gmail.com>
2023-03-16 08:34:20 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*
- * main -print exact statement using put function
- * return :alway 0
- */
-int main(void)/*declaration of function*/
+/**
+ * main -print exact statement using put functio
+ * 
+ * Return : 0
+*/
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
