c835728a15068b82368825ada6cb5f77081c0f7e
RaveenaBora <raveenabora4@gmail.com>
2023-03-20 10:23:30 -0700

    print  combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: always 0
- */
+  *main - entry point
+  *Description: "print combination of numbers"
+  *Return: always 0
+  *
+  *Declare variable n that will act as  counter
+  *Write a for loop to loop through 0-9
+  *print the numbers
+  *Write an if statement
+  *print new line
+  */
 int main(void)
 {
-	int i, j;
-		for (i = 0; i < 10; i++)
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n < '9')
 		{
-			for (j = i; j < 10; j++)
-			{
-				putchar(i + '0');
-				putchar(',');
-				putchar(' ');
-				putchar(j + '0');
-				if (i != 9 || j != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
+	}
+	putchar('\n');
 	return (0);
 }
+
+
+
