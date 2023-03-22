f843eb0bea5611455b935e753fbce8b49bb3ecc8
RaveenaBora <raveenabora4@gmail.com>
2023-03-22 12:54:27 -0700

    print time

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,18 @@
 /**
  * print_last_digit - prints the last digits of any number
  * @a: the int value to be acted upon
- * author: sammykingx
  * Return: returns the last digit to calling function
 */
 int print_last_digit(int a)
 {
-	if (a >= 0 && a <= 9)
-		return (a);
+	int a;
 
-	else
+	a = n % 10;
+	if (a < 0)
 	{
-		a = _abs(a % 10);
-		return (a);
+		a = a * -1;
+	}
+	-putchar(a * '0');
+	return (a);
 	}
 }
